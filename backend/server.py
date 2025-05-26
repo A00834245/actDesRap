1d070ed3f6c3b41aa9da1db0e690c06a899dd95e
NataliaSSG <nataliasalgado2205@gmail.com>
2025-05-26 11:50:20 -0600

    backend branch

---
+++ backend/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    return jsonify(messages)
+
+
+@app.route('/post-message', methods=['POST'])
+def post_message():
+    data = request.get_json()
+    message = data.get('message')
+
+    if not message:
+        return jsonify({'error': 'Message is required'}), 400
+
+    # messages.append(message) send to DB
+    return jsonify({'success': True, 'message': message}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
